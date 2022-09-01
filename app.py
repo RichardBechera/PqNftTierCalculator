f02eca770519e4482e3f86dc4e46babed8554808
RichardBechera <rikk0011@gmail.com>
2022-09-01 15:09:04 +0200

    my head hurts

--- app.py
+++ app.py
@@ -1,37 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from TierCalculator import qualculate
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def adder_page():
-    if request.method == "POST":
+    if request.method == 'POST':
         address = request.form["address"]
         apiKey = request.form["apiKey"]
         if address is not None and apiKey is not None:
             result = qualculate(address, apiKey)
-            return '''
-                <html>
-                    <body>
-                        <p>The result is {result}</p>
-                        <p><a href="/">Click here to calculate again</a>
-                    </body>
-                </html>
-            '''.format(result=result)
-    return '''
-        <html>
-            <body>
-                <p>Enter your information:</p>
-                <form method="post" action=".">
-                    <p><input name="address" /></p>
-                    <p><input name="apiKey" /></p>
-                    <p><input type="submit" value="Qualculate!" /></p>
-                </form>
-            </body>
-        </html>
-    '''
+            return render_template('result.html', name=result)
+    return render_template('form.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)
